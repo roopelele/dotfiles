d00e15737448454207ec07beb2f8caf1a02f44eb
roope <roope.jarvelainen@gmail.com>
2021-02-12 15:34:45 +0200

    script now completely reinstalls oh-my-zsh every time it is used

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 dotfiles_dir=~/dotfiles
 
-cat packages.txt | xargs sudo apt install
+cat packages.txt | xargs sudo apt install -y
 pip3 install -r pip-packages.txt
 
 while true; do
     read -p "Do you want to install oh-my-zsh y/n " yn
     case $yn in
-        [Yy]* ) sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"         break;;
+        [Yy]* ) sh -c "$(rm -rf ~/.oh-my-zsh && wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"         break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
+rm -rf ~/.zshrc
 cp $dotfiles_dir/.zshrc ~
 
 echo "Install finished"
